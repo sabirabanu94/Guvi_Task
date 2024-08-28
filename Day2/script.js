0f45e960db232f92b785c6db7d8e982a1408da11
sabirabanu94 <128887202+sabirabanu94@users.noreply.github.com>
2024-08-28 20:06:17 +0530

    script.js

---
+++ Day2/script.js
@@ -0,0 +1,40 @@
+// alert("Hi!!!!Welcome to my page!")
+
+// let names = prompt("Enter you name");
+// alert("Hi!!!!" + names);
+
+// let a='Sabira'; //var--> string
+// let b=' 01';  //var--> string
+// console.log(a+b);
+
+// let a=10.5; // float
+// let b=5;   // number
+
+//  console.log(a+b);
+
+
+
+// let a= true; // boolean
+// let b=false; // boolean
+
+// console.log(a && b);
+// console.log(a || b);
+// console.log(!b)
+
+
+// let names='Sabira';
+// let age=29;
+// const place="Chennai";
+
+// console.log(names + " from " + place + " aged " + age);
+
+
+let pr = {
+    "Salutation": "MRS",
+    "Name": "Sabira",
+    "Age": 29,
+    "Place": "Chennai",
+    "Affiliation": "AMS Technology",
+    "Phone": 9876543212
+}
+console.log(pr);
