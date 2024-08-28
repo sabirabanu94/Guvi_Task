ba44d98589fc7df25152475834fbec553dec414e
sabirabanu94 <128887202+sabirabanu94@users.noreply.github.com>
2024-08-28 20:09:36 +0530

    Add files via upload

---
+++ Day3/script.js
@@ -0,0 +1,99 @@
+// function myfunction (){
+//     console.log ("Hi all");
+// }
+// myfunction();
+
+
+// function myfunc(Coder){
+
+//     return function (invitee){
+//         console.log (invitee+ " " + Coder)
+//     }
+// }
+// const retfunc=myfunc("Sabira");
+// retfunc("Welcome");
+
+// function myfunc(callback) {
+//     const name = "Sabira";
+//     callback(name);
+// }
+
+// myfunc(function(name) {
+//     console.log("Welcome!! " + name);
+// });
+
+// function top(){
+//     console.log("Top function");
+//     return function bottom(){
+//         console.log ("Bottom function");
+//     }
+// }
+// let a=top();
+// a();
+
+// ===============================================================
+
+// let a = 9;
+// if (a >= 9.8) {
+
+//     console.log("Condition Satisfies");
+// } else {
+//     console.log("condition fails");
+// }
+
+// let a = 10;
+// if (a >= 8) {
+//     console.log("first conditon satisfies");
+//     if (a < 10) {
+//         console.log("second conditon satisfies"); 
+//     }else{
+//         console.log("second condition fails");
+//     }
+//     if (a=10){
+//         console.log("Third conditon satisfies"); 
+//     }else{console.log("thirf condition fails")}
+//     let b=(a<10) ? "condtion satisfies" : "condition fails"
+//     console.log(b);
+//     let c=(b=null) ? "True": "False"
+//     console.log(c)
+
+// }
+
+// function myfunc() {
+//     let a;
+//     let mychoice = prompt('Which your fav city :');
+//     switch (mychoice) {
+//         case 'chennai':
+//             alert("Chennai is the city of opportunity");
+//             break;
+//         case 'mumbai':
+//             alert("City of dreams");
+//             break;
+//         case 'coimbatore':
+//             alert("Manchester of Tamil");
+//             break;
+//         case 'Goa':
+//             alert("City to party");
+//             break
+//         default:
+//             alert("Bon-voyage");
+//             break;
+//     }
+// }
+// myfunc();
+
+// let looping = 0;
+// while (looping < 10) {
+//     console.log("looping start :" + looping);
+//     looping++;
+// }
+
+// let loop=10;
+// do{
+//     console.log("looping end :" + loop);
+//     loop=loop++;
+// }
+//     while (loop < 10) {
+//      console.log("looping starts :" + loop);   
+//     }
+
